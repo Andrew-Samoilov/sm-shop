e64808b8bf8a9d2fc1edd1cda1578bdd70696458
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-03-19 17:28:38 +0200

    refactor: 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,14 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <section className="flex flex-col items-center justify-center min-h-full text-center">
+      <p
+        className="pb-[0.25em] text-7xl md:text-9xl lg:text-[192px] font-extrabold"
+        style={{
+          WebkitTextStroke: "2px #4169e1",
+          // через accent не працює :-(
+          color: "transparent",
+        }}
+      >404!</p>
       <h1>Сторінка не знайдена</h1>
       <p className="md:mb-8 text-lg">
         Нажаль, за цією адресою сторінки не існує або вона була переміщена.
