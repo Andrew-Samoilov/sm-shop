e1a8005a4d93f82b44507f0af59514a4d51a1371
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-05-09 15:53:24 +0300

    feat: brands

--- src/app/warranty/page.tsx
+++ src/app/warranty/page.tsx
@@ -4,7 +4,7 @@ import ReactMarkdown from "react-markdown";
 export default async function WarrantyPage() {
   const content = await getMdContent("warranty");
   return (
-    <section className="">
+    <section className="lg:max-w-[70ch]">
       <ReactMarkdown>{content}</ReactMarkdown>
     </section>
   );
