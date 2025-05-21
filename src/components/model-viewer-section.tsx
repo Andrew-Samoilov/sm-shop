6f42ead2a03e51c1c383d827da1bca0b0b2c52c2
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-05-21 14:56:47 +0300

    refactor: model viewer section

--- src/components/model-viewer-section.tsx
+++ src/components/model-viewer-section.tsx
@@ -17,7 +17,9 @@ export function ModelViewerSection({
     position: number;
   }[];
 }) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ align: "start", loop: true });
+  const isSingle = images.length === 1;
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ align: "start", loop: !isSingle, });
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const scrollTo = useCallback((index: number) => emblaApi?.scrollTo(index), [emblaApi]);
@@ -35,65 +37,83 @@ export function ModelViewerSection({
     onSelect();
   }, [emblaApi, onSelect]);
 
+  const sortedImages = images.toSorted((a, b) => (a.position) - (b.position));
+
   return (
     <section className="relative w-full overflow-hidden m-0 p-0 ml-auto lg:h-[50vh]  2xl:w-3/4 ">
-      <div className="flex flex-col lg:flex-row gap-6  h-full 2xl:items-center">
-        <button
-          onClick={scrollPrev}
-          className="hidden lg:block z-10"
-          aria-label="Попереднє зображення"
-        >
-          <ArrowLeftCircleIcon
-            className="text-light h-10 w-10 cursor-pointer hover:text-dark
-                       dark:text-darkmode-light dark:hover:text-darkmode-dark" />
-        </button>
 
-        {/* Viewport */}
-        <div className="relative w-full overflow-hidden h-full" ref={emblaRef}>
-          <div className="flex">
-            {images.map(({ id, url, alt, width, height }, index) => (
-              <div key={id} className="relative flex-[0_0_100%] h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] 2xl:h-[600px]">
-                <Image
-                  src={url}
-                  alt={alt ?? "Фото моделі"}
-                  width={width ?? 800}
-                  height={height ?? 600}
-                  priority={index === 0}
-                  className="h-full w-full object-contain"
-                />
-              </div>
+      {isSingle ? (
+        <div className=" max-w-[640px] mx-auto px-4 aspect-[3/4]">
+          <Image
+            key={images[0].id}
+            src={images[0].url}
+            alt={images[0].alt ?? "Фото моделі"}
+            fill
+            priority
+            sizes="(min-width: 1280px) 25vw, (min-width: 1024px) 33.33vw, (min-width: 768px) 50vw, 100vw"
+            className="object-contain"
+          />
+        </div>
+      ) : (
+
+        <div className="flex flex-col lg:flex-row gap-6  h-full 2xl:items-center">
+          <button
+            onClick={scrollPrev}
+            className="hidden lg:block z-10"
+            aria-label="Попереднє зображення"
+          >
+            <ArrowLeftCircleIcon
+              className="text-light h-10 w-10 cursor-pointer hover:text-dark
+                       dark:text-darkmode-light dark:hover:text-darkmode-dark " />
+          </button>
+
+          {/* Viewport */}
+          <div className="relative w-full overflow-hidden h-full" ref={emblaRef}>
+            <div className="flex">
+              {sortedImages.map(({ id, url, alt, width, height }, index) => (
+                <div key={id} className="relative flex-[0_0_100%] h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] 2xl:h-[600px]">
+                  <Image
+                    src={url}
+                    alt={alt ?? "Фото моделі"}
+                    width={width ?? 800}
+                    height={height ?? 600}
+                    priority={index === 0}
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            onClick={scrollNext}
+            className="hidden lg:block z-10  "
+            aria-label="Наступне зображення"
+          >
+            <ArrowRightCircleIcon
+              className="text-light h-10 w-10 cursor-pointer hover:text-dark
+                       dark:text-darkmode-light dark:hover:text-darkmode-dark" />
+          </button>
+
+          {/* Thumbnails */}
+          <div className="flex flex-row gap-2 lg:gap-6 overflow-x-auto lg:py-2 lg:flex-col ">
+            {sortedImages.map((img, index) => (
+              <Image
+                key={img.id}
+                src={img.url}
+                alt={img.alt ?? "Прев’ю"}
+                width={0}
+                height={0}
+                sizes="6rem"
+                className={`w-15 h-12 lg:w-25 lg:h-20 object-contain
+                rounded border-2 transition-all shrink-0 cursor-pointer ${index === selectedIndex ? "opacity-100 border-accent" : "opacity-50 border-transparent"
+                  }`}
+                onClick={() => scrollTo(index)}
+              />
             ))}
           </div>
-        </div>
+        </div>)}
 
-        <button
-          onClick={scrollNext}
-          className="hidden lg:block z-10  "
-          aria-label="Наступне зображення"
-        >
-          <ArrowRightCircleIcon
-            className="text-light h-10 w-10 cursor-pointer hover:text-dark
-                       dark:text-darkmode-light dark:hover:text-darkmode-dark" />
-        </button>
-
-        {/* Thumbnails */}
-        <div className="flex flex-row gap-2 lg:gap-6 overflow-x-auto lg:py-2 lg:flex-col ">
-          {images.map((img, index) => (
-            <Image
-              key={img.id}
-              src={img.url}
-              alt={img.alt ?? "Прев’ю"}
-              width={0}
-              height={0}
-              sizes="6rem"
-              className={`w-15 h-12 lg:w-25 lg:h-20
-                rounded border-2 transition-all shrink-0 cursor-pointer ${index === selectedIndex ? "opacity-100 border-accent" : "opacity-50 border-transparent"
-                }`}
-              onClick={() => scrollTo(index)}
-            />
-          ))}
-        </div>
-      </div>
     </section>
   );
 }
