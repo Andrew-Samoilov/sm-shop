1daf84e659e1885663ac6f299944af6c488f307a
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-05-27 18:00:18 +0300

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
-import { TyresList } from "@/components";
+import { TyresList, TyresSelect } from "@/components";
 import { getTyresFromApi } from "@/lib";
 
 export default async function Home({ searchParams }: { searchParams: Record<string, string> }) {
   const width = typeof searchParams.width === "string" ? searchParams.width : "";
   const profile = typeof searchParams.profile === "string" ? searchParams.profile : "";
   const diameter = typeof searchParams.diameter === "string" ? searchParams.diameter : "";
-  const seasons = Array.isArray(searchParams.season)
-    ? searchParams.season
-    : searchParams.season
-      ? [searchParams.season]
-      : [];
-  
+
+  const rawSeason = await searchParams.season;
+  let seasons: string[] = [];
+
+  if (Array.isArray(rawSeason)) {
+    seasons = rawSeason;
+  } else if (typeof rawSeason === "string") {
+    seasons = [rawSeason];
+  }
+
   const view = await searchParams.view === "gallery" ? "gallery" : "list";
 
-  
   const data = await getTyresFromApi({ width, profile, diameter, seasons });
 
   return (
     <section className="flex justify-center flex-wrap from-body to-theme-light dark:from-darkmode-body dark:to-darkmode-theme-light bg-gradient-to-b
         w-full p-6 lg:p-0 flex-col gap-6 items-center">
 
-      {/* <TyresSelect searchParams={searchParams} /> */}
+      <TyresSelect />
       <TyresList tyres={data.tyres} images={data.images} view={view} />
     </section>
   );
