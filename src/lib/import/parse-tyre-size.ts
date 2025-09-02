37f76351367e40b125a8f84ae315bbf857df29b4
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-09-02 14:28:16 +0300

    refactor: parse tyre size

--- src/lib/import/parse-tyre-size.ts
+++ src/lib/import/parse-tyre-size.ts
@@ -1,27 +1,26 @@
 export function parseTyreSize(size: string) {
   if (!size) return null;
   const s = size.trim()
-
-  // 1) Класичний формат: 205/55R16, LT225/75R16C
-  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})(?:C|LT)?/i;
-  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})/i;
-  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([\/-])(\d{2})(Z?R|D)(\d{2})(?:C|XL|LT)?/i;
-  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})/i;
-  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})(R|D)(\d{2})\b/i;
   
   const reLT = /^(?:LT|HL)?\s*(\d{3})[/-](\d{2})([RD])(\d{2})$/i;
   let m = reLT.exec(s)
   if (m) {
     console.log('Matched LT format', m);
     return {
-      width: parseInt(m[1], 10),
-      delimiter: m[2],
-      profile: parseInt(m[3], 10),
-      constr: m[4].toUpperCase(),
-      diameter: parseInt(m[5], 10),
-    }
+      width: parseInt(m[1], 10),  
+      profile: parseInt(m[2], 10), 
+      constr: m[3].toUpperCase(),  
+      diameter: parseInt(m[4], 10) 
+    };
   }
   
+  // 1) Класичний формат: 205/55R16, LT225/75R16C
+  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})(?:C|LT)?/i;
+  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})/i;
+  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([\/-])(\d{2})(Z?R|D)(\d{2})(?:C|XL|LT)?/i;
+  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})([RD])(\d{2})/i;
+  // const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})(R|D)(\d{2})\b/i;
+
   const reClassic = /^(?:LT|HL|P|T)?\s*(\d{3})([/-])(\d{2})(RD)(\d{2})$/i;
   m = reClassic.exec(s)
   if (m) {
