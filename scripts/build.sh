d71913c3b24be8f8144b450ffe1f64a932a69b44
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-09-16 17:38:54 +0300

    feat: build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,13 @@ set -e
 LOG_OUT="/root/.pm2/logs/shina-mix-shop-out.log"
 LOG_ERR="/root/.pm2/logs/shina-mix-shop-error.log"
 
-echo "=== Початок білда після імпорту ==="
+echo "[build] Початок білда після імпорту"
 cd /var/www/shina-mix-shop
+
+echo "[build] generate prisma client..."
+npx prisma generate
 
 npm run build
 pm2 restart shina-mix-shop
 
-echo "=== Білд завершено ==="
+echo "[build] Білд завершено "
