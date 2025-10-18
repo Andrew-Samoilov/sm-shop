6d5beef05405a17dbf550e12d1e3e66446053e6c
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-10-18 20:24:28 +0300

    feat: ga event from top baner

--- src/components/working-hours-phone.tsx
+++ src/components/working-hours-phone.tsx
@@ -1,10 +1,9 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-import { getSiteConfig } from "@/lib";
-
+import { getSiteConfig, sendGAEvent } from "@/lib";
+import { PhoneIcon } from "@heroicons/react/24/outline";
 
 export default async function WorkingHoursPhone() {
-    // if (process.env.NODE_ENV !== 'production') return null;
     const siteConfig = await getSiteConfig();
 
     // Поточний час на сервері (Europe/Berlin)
@@ -12,16 +11,46 @@ export default async function WorkingHoursPhone() {
 
     // Київський час = серверний + 1 година
     const kyivHour = (now.getUTCHours() + 3) % 24;
+    const day = now.getUTCDay();
 
-    // Робочі години Києва: з 10:00 до 18:00
-    const isWorkingHours = kyivHour >= 10 && kyivHour < 18;
+    let isWorkingHours: boolean;
+
+    switch (day) {
+        case 0:
+            // Неділя — вихідний
+            isWorkingHours = false;
+            break;
+        case 6:
+            // Субота: 10:00–15:00
+            isWorkingHours = kyivHour >= 10 && kyivHour < 15;
+            break;
+        default:
+            // Пн–Пт: 10:00–18:00
+            isWorkingHours = kyivHour >= 10 && kyivHour < 18;
+            break;
+    }
 
     if (!isWorkingHours) return null;
+
+    const handleClick = () => {
+        if (typeof globalThis !== "undefined") {
+            sendGAEvent({
+                action: "click_phone_banner",
+                category: "engagement",
+                label: "WorkingHoursPhone",
+                value: kyivHour,
+            });
+        }
+    };
+
 
     return (
         <a
             className="flex items-center justify-center text-center md:hidden py-2 bg-theme-light dark:bg-theme-dark"
-            href={ siteConfig.tel.normalize} >
+            href={siteConfig.tel.normalize}
+            onClick={handleClick}
+        >
+            <PhoneIcon className=" h-4 w-4 mr-2" />
             {siteConfig.tel.visual}
         </a>
     );
