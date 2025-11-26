1eab50e3e9b13c40e2593fc2033ef66913608dec
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-11-26 10:46:37 +0200

    fix: info section

--- src/components/info-section.tsx
+++ src/components/info-section.tsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-static";
 import { getInfoPages } from "@/lib/server/get-info-pages";
 import Link from "next/link";
+import Image from "next/image";
 
 export async function InfoSection() {
   const pages = await getInfoPages();
@@ -11,6 +12,13 @@ export async function InfoSection() {
         href="/info"
         className="text-center text-lg font-semibold  text-light hover:text-dark dark:text-darkmode-text dark:hover:text-darkmode-primary hover:no-underline"
       >
+        <Image
+          src='/others/info-page-2.png'
+          alt="Інформаційні сторінки"
+          width={300}
+          height={300}
+          className="mx-auto rounded-md"
+        />
         <h2 className="text-center pb-6">Інформаційні сторінки</h2>
       </Link>
       <ul className="flex flex-col md:flex-row flex-wrap gap-2  list-none justify-center">
